cd6dc1556a57e9a5b3e0fc9309a04e6399285b5b
KiraLer0912 <134572331+KiraLer0912@users.noreply.github.com>
2026-03-03 13:46:49 +0800

    Small bug fixing and addition of stuff
    -Fix my camera movement tilting my capsule
    -Added sprint and jump

--- Assets/Scripts/FPSInput.cs
+++ Assets/Scripts/FPSInput.cs
@@ -6,8 +6,11 @@ public class FPSInput : MonoBehaviour
 {
     public float speed = 6.0f;
     public float gravity = -9.8f;
+    public float sprintSpeed = 12.0f;
+    public float jumpForce = 5.0f;
 
     private CharacterController charController;
+    private float verticalVelocity;
 
     void Start()
     {
@@ -17,17 +20,28 @@ public class FPSInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float deltaX = Input.GetAxis("Horizontal") * speed;
-        float deltaZ = Input.GetAxis("Vertical") * speed;
+        //Added Sprint
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : speed;
+        float deltaX = Input.GetAxis("Horizontal") * currentSpeed;
+        float deltaZ = Input.GetAxis("Vertical") * currentSpeed;
+
         Vector3 movement = new Vector3(deltaX, 0, deltaZ);
-        movement = Vector3.ClampMagnitude(movement, speed);
+        movement = Vector3.ClampMagnitude(movement, currentSpeed);
+
+        //Added Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            verticalVelocity = jumpForce; // instantly set upward velocity
+        }
+
 
         // Convert to world space first
-        movement = transform.TransformDirection(movement);
+        movement = Camera.main.transform.TransformDirection(movement);
 
         // Apply gravity straight down in world space
-
-        movement.y = gravity;
+        // --- Gravity ---
+        verticalVelocity += gravity * Time.deltaTime;
+        movement.y = verticalVelocity;
 
         charController.Move(movement * Time.deltaTime);
     }
