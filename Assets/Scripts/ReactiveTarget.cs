bac86f315c3ea154ed0ea9933e995c9a5418c569
KiraLer0912 <134572331+KiraLer0912@users.noreply.github.com>
2026-03-03 13:30:05 +0800

    Practical 3
    -done practical 3
    -edited camera clipping planes to 0.01 so that the camera wont look over walls when looking down

---
+++ Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class ReactiveTarget : MonoBehaviour
+{
+    public void ReactToHit()
+    {
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        this.transform.Rotate(-75, 0, 0);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(this.gameObject);
+    }
+}
