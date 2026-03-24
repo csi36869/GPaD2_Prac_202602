0815b07afb477704b920e39fbcfbf28d589db1de
zhenghan0208 <zhenghanlam@gmail.com>
2026-03-24 13:21:50 +0800

    prac 7.4.2
     score display when you shoot
    an enemy

--- Assets/Scripts/RayShooter.cs
+++ Assets/Scripts/RayShooter.cs
@@ -37,6 +37,7 @@ public class RayShooter : MonoBehaviour
                 if ( target != null)
                 {
                     target.ReactToHit();
+                    Messenger.Broadcast(GameEvent.ENEMY_HIT);
                 }
                 else
                 {
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -8,17 +8,38 @@ public class UIController : MonoBehaviour
     [SerializeField] TMP_Text scoreLabel;
     [SerializeField] SettingsPopup settingsPopup;
 
+    private int score;
+
+    void OnEnable()
+    {
+        Messenger.AddListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
+    void OnDisable()
+    {
+        Messenger.RemoveListener(GameEvent.ENEMY_HIT, OnEnemyHit);
+    }
+
     private void Start()
     {
+        score = 0;
+        scoreLabel.text = score.ToString();
+
         settingsPopup.Close();
     }
 
     void Update()
     {
-        scoreLabel.text = Time.realtimeSinceStartup.ToString();
+
     }
     public void OnOpenSettings()
     {
         settingsPopup.Open();
+    }
+
+    private void OnEnemyHit()
+    {
+        score += 1;
+        scoreLabel.text = score.ToString();
     }
 }
